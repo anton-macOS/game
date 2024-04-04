0bc2cd9af5a55f61b0e06950d66be69e0e64af9d
Anton <anton.kiriliuk@gmail.com>
2024-04-04 23:48:57 +0300

    Added function with request of attempts and checking word coincidence

--- main.py
+++ main.py
@@ -3,3 +3,27 @@ from random import choice
 word = ['apple', 'orange', 'pineapple', 'watermelon', 'melon', 'grape']
 
 choice = choice(word)
+
+def game(word: str):
+    attempts = int(input('Скільки буде спроб: '))
+    count_attempts = attempts - 1
+    while True:
+        guess_letter = input('Введіть букву або слово: ')
+        if count_attempts == 0:
+            print('Програли')
+            break
+        if len(guess_letter) > 1:
+            if guess_letter == word:
+                print('Вгадали')
+                break
+            else:
+                print('Неправильно')
+                count_attempts -= 1
+
+
+game(choice)
+
+
+
+
+
