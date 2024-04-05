b3315d3bba9d81821b14af63e9627fb0888dcfac
Anton <anton.kiriliuk@gmail.com>
2024-04-05 15:35:51 +0300

    Added game functianality, added .gitignore, changed structure of function

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 from random import choice
 
-word = ['apple', 'orange', 'pineapple', 'watermelon', 'melon', 'grape']
+# word = ['apple', 'orange', 'pineapple', 'watermelon', 'melon', 'grape']
+word = ['pineapple']
 
 choice = choice(word)
 
-def game(word: str):
-    attempts = int(input('Скільки буде спроб: '))
-    count_attempts = attempts - 1
-    while True:
+
+def game(word: str, attempts: int):
+    secret_word = ['*'] * len(word)
+    win_word = ''.join(secret_word)
+    print(f'Загадано слово: {win_word}')
+    while attempts > 0:
         guess_letter = input('Введіть букву або слово: ')
-        if count_attempts == 0:
-            print('Програли')
-            break
         if len(guess_letter) > 1:
             if guess_letter == word:
                 print('Вгадали')
                 break
             else:
                 print('Неправильно')
-                count_attempts -= 1
+        elif guess_letter in word:
+            for index, letter in enumerate(word):
+                if guess_letter == letter:
+                    secret_word[index] = letter
+                    win_word = ''.join(secret_word)
+            print(win_word)
+            if '*' not in secret_word:
+                print('Вгадали')
+                break
+        elif guess_letter not in word:
+            print('Цієї літери нема')
+        attempts -= 1
 
-
-game(choice)
+attempts = int(input('Скільки буде спроб: '))
+game(choice, attempts)
 
 
 
